7875a6d078578c0031a5f50f7ee23d013517a0fb
Zahraa753 <zahraahassan075@gmail.com>
2023-10-05 11:46:08 -0700

    new

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * str_concat - main used function
+ *
+ * @s1: first text
+ * @s2: second text
+ *
+ * Return: the tatal value
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+	char *ptr;
+	char *pon;
+
+	for (; s1[i] != '\0'; i++)
+		;
+	ptr = malloc(i)
+		for (i = 0; s1[i] != 0; i++)
+		{
+			ptr[i] = s[i];
+		}
+	for (; s2[j] != 0; j++)
+		;
+
+	pon = realloc(ptr, j);
+	for (j = 0; s2[j] '\0'; j++; i++)
+	{
+		pon[j] == ptr[i];
+	}
+	return (pon);
+}
