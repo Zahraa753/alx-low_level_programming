53625e48cdc24bb043857859a1a980f8f991885b
Zahraa753 <zahraahassan075@gmail.com>
2023-10-05 18:22:12 -0700

    bad

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 
 	for (; s1[i] != '\0'; i++)
 		;
-	if (s[i] == NULL)
+	if (s1[i] == NULL)
 		return (NULL);
-	ptr = malloc(i + 1);
+	ptr = malloc(i);
 		for (i = 0; s1[i] != '\0'; i++)
 		{
 			ptr[i] = s1[i];
@@ -29,7 +29,7 @@ char *str_concat(char *s1, char *s2)
 		;
 	if (s2[j] == NULL)
 		return (NULL);
-	pon = realloc(ptr, j + 1);
+	pon = realloc(ptr, j);
 	for (j = 0; s2[j] != '\0'; j++, i++)
 	{
 		pon[i] = s2[j];
