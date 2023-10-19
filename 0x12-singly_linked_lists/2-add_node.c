b5e0ae45b80c4ebe9861f8ade37f56beb8e9b011
Zahraa753 <zahraahassan075@gmail.com>
2023-10-19 14:02:55 -0700

    task of adding node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_node - the used functionto add element
+ * @head: the passed address
+ * @str: the passed new node
+ *
+ * Return: the value of the length of str
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *new = malloc(sizeof(list_t));
+
+	if (!head || !new)
+		return (NULL);
+	if (str)
+	{
+		new->str = strdup(str);
+		if (!new->str)
+		{
+			free(new);
+			return (NULL);
+		}
+		new->len = -strlen(new->str);
+	}
+/**
+ * modify the address
+*/
+	new->next = *head;
+	*head = new;
+	return (new);
+}
