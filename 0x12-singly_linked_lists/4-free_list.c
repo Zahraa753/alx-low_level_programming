abc58b702436b32aecc8690073042afb1dbefcf0
Zahraa753 <zahraahassan075@gmail.com>
2023-10-20 13:45:34 -0700

    writing base

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_list - the main function
+ * @head: the passed pointer
+ *
+ * Return: NOTHING
+*/
+
+void free_list(list_t *head)
+{
+	list_t *node;
+	list_t *cont;
+
+	if (!head)
+		return;
+	node = head;
+	while(node)
+	{
+		cont = node->next;
+		free(node->str);
+		free(node);
+		node = cont;
+	}
+}
+
