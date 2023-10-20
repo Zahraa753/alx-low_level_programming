c99267b63a13d514ef8a4838d7c7edf3ae4cc231
Zahraa753 <zahraahassan075@gmail.com>
2023-10-19 17:45:26 -0700

    putting element at the last

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,7 +14,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *last = malloc(sizeof(list_t));
 	list_t *com = *head;
 
-	if (!head || !str)
+	if (!head || !las)
 	{
 		return (NULL);
 	}
