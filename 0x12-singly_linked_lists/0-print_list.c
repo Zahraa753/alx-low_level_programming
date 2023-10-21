f6b727fae7459526ea8b8b5840c701fa4db88f72
Zahraa753 <zahraahassan075@gmail.com>
2023-10-21 03:01:37 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,6 +3,24 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - this function to givr=e me the numbers of elements in s
+ * @s: this is the passed data
+ *
+ * Return: the value of j
+*/
+
+int _strlen(char *s)
+{
+	int j = 0;
+
+	if (!s)
+		return (NULL);
+	while (*s++)
+		j++;
+	return (j);
+}
+
+/**
  * print_list - the function used to print all elements
  * @h: the passed hedder
  *
@@ -18,7 +36,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %S\n", h->len, h->str);
+			printf("[%d] %S\n", _strlen(h->len), h->str);
 		node++;
 		h = h->next;
 	}
