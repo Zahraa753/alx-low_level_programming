0912cf6a53b2b8b5c5da6cf93f826e0ecaf235b9
Zahraa753 <zahraahassan075@gmail.com>
2023-10-21 12:09:55 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,10 +6,11 @@ void _constructor(void) __attribute__ ((constructor));
  * _constructor - print before main() be excuted
  *
  * Return: nothing
-*/
+ */
 
 void _constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n""I bore my house upion my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upion my back!\n");
 }
 
