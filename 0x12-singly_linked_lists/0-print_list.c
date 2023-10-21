8c25643a0743be6090942154469cdcebe41b5f18
Zahraa753 <zahraahassan075@gmail.com>
2023-10-21 01:18:19 -0700

    all

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,39 +1,3 @@
 #include "lists.h"
 
-/**
- * _strlen - this function to givr=e me the numbers of elements in s
- * @s: this is the passed data
- *
- * Return: the value of j
-*/
 
-int _strlen(char *s)
-{
-	int j = 0;
-
-	if (!s)
-		return (NULL);
-	while (*s++)
-		j++;
-	return (j);
-}
-
-/**
- * print_list - the base function that prints members of the h array
- * @h: the passed data
- *
- * Return: the number of nodes
-*/
-
-size_t print_list(const list_t *h)
-{
-	size_t j = 0;
-
-	while (h)
-	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-		h = h->next;
-		j++;
-	}
-	return (j);
-}
