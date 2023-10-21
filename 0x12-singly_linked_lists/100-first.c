8b4cdaf71e39a13039a510810fc4cadc1666ddd2
Zahraa753 <zahraahassan075@gmail.com>
2023-10-21 10:38:13 -0700

    aconstructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - print before main() be excuted
+ *
+ * Return: void
+*/
+
+void _constructor(void)
+{
+	prinf("You're beat! and yet, you must allow,\n
+			I bore my house upion my back!\n");
+}
+
