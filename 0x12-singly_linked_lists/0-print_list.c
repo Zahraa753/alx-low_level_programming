e47ad3d4a6796e68025e9e5064ca17fc5cd434cb
Zahraa753 <zahraahassan075@gmail.com>
2023-10-21 01:31:07 -0700

    change code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,3 +1,24 @@
 #include "lists.h"
 
+/**
+ * print_list - the function used to print all elements
+ * @h: the passed hedder
+ *
+ * Return: the number of nodes
+*/
 
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %S\n", h->len, h->str);
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
