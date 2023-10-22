c409bae0f442ce3b6c2b2919f4d2d12143a817a9
Zahraa753 <zahraahassan075@gmail.com>
2023-10-22 12:11:24 -0700

    changing the betty

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main _ Entry point
+ * @argc: the number of named
+ * @argv: the named
+ *
+ * Description: print the number of bassed values
+ *
+ * Return: success 0
+*/
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
