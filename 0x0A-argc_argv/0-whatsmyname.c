e64c077c89114d6fe390c28e5d208fce8a36215a
Zahraa753 <zahraahassan075@gmail.com>
2023-10-22 12:02:43 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: the num of argv
+ * @argv: print the name of excution
+ *
+ * Description: print the name of ecxution
+ *
+ * Return: Success (0)
+*/
+
+int main(int argc __attribute__ ((unused)),char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
