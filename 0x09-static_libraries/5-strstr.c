cc5eb0429d0ac12e33cbf2b0990adbb83618b0dc
Zahraa753 <zahraahassan075@gmail.com>
2023-10-22 11:45:21 -0700

    the readme , and min file

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; haystack != '\0'; i++)
 	{
 		if ( needle[j] == haystack[i])
 		{
