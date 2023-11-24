8377692ebca95cb35b82bfe69c6afb4214839650
Zahraa753 <zahraahassan075@gmail.com>
2023-11-24 14:44:50 +0200

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_puts_recursion-the fuction prints the words
+*@s: the used variable
+*Description: prints words
+*Return: nothing
+*/
+
+void _puts_recursion(char *s)
+{
+	_putchar(*(s++));
+	if (*s != '\0')
+	_puts_recursion(s);
+	else
+	_putchar('\n');
+}
