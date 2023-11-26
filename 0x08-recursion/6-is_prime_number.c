21ae8a8a18d172ab681615dc2db308aaec354262
Zahraa753 <zahraahassan075@gmail.com>
2023-11-26 23:55:02 +0200

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*is_prime_number- determine if number is prime or not
+*@n: the passed number
+*Description: get the prime number
+*Return: 1 or 0
+*/
+
+int is_prime_number(int n)
+{
+
+	return (helpfunc(n, 2));
+}
+
+/**
+*helpfunc- help me in proccess
+*@n:the passed num
+*@val:the compared num
+*Description: help me to compare
+*Return: 1 or 0
+*/
+
+int helpfunc(int n, int val)
+{
+	if (val >= n && n > 1)
+	return (1);
+	else if (n % val == 0 || n <= 1)
+	return (0);
+	else
+	return (helpfunc(n, val + 1));
+
+}
