4676124574cca44efd3058aac6949403426a6e5b
Zahraa753 <zahraahassan075@gmail.com>
2023-11-26 23:24:06 +0200

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+*_sqrt_recursion- gives me the square root of number
+*@n: the vairable 1
+*Description: like sqr() function
+*Return: Nothing
+*/
+
+int _sqrt_recursion(int n)
+{
+	return (helpfunc(n, 1));
+}
+
+/**
+*helpfunc- help me to recursion
+*@n: the passed value of num
+*@val: the proccessed passed value
+*Description: help me in proccess
+*Return: Nothing
+*/
+
+int helpfunc(int n, int val)
+{
+	if (val * val == n)
+	return (val);
+	else if (val * val < n)
+	return (helpfunc(n, val + 1));
+	else
+	return (-1);
+}
+
