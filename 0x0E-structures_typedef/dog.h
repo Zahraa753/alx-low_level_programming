f6a750664f68d097d1cd7929ffe8a06a36882f7a
Zahraa753 <zahraahassan075@gmail.com>
2023-11-28 21:45:56 +0200

    all

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+*struct dog- new struct help me geting poppoes
+*@name: child name
+*@age: child age
+*@owner: the owner
+*Description: it is like object
+*Return: nothing
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
