411aeb4637b8a840e060557b09b0d6e7f8dd13c3
Zahraa753 <zahraahassan075@gmail.com>
2023-11-28 21:53:19 +0200

    all

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
 *Description: it is like object
 *Return: nothing
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
