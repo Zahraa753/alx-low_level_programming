5bc0b173a16c59a02ee2a18d9ce0f9f69cdb56d8
Zahraa753 <zahraahassan075@gmail.com>
2023-11-30 15:09:57 +0200

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef _HEAD_H
 #define _HEAD_H
+#include <stdio.h>
 
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
 
 #endif
