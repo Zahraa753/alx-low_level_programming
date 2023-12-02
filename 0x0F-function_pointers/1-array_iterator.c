aa19c4fbc01e23c0cce67a6719da858570b298d4
Zahraa753 <zahraahassan075@gmail.com>
2023-12-02 06:17:03 +0200

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
+
 /**
 *array_iterator- the used function
 *@array: the passed array
 *@size: the passed size of array
 *@action: the pointer to array
 */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
