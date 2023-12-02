1a5aee7aa19b846f4414d513aa68e905adaf298a
Zahraa753 <zahraahassan075@gmail.com>
2023-12-02 11:50:12 +0200

    all

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
 #ifndef _HEAD_H
 #define _HEAD_H
 #ifndef size_t
-#define size_t long unsigned int
+#define size_t
+typedef long unsigned int size_t
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
