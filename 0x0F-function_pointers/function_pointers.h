6029539cd445de2002f164f2a1a894abb73d2220
Zahraa753 <zahraahassan075@gmail.com>
2023-12-02 11:51:51 +0200

    includeing lib

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,7 @@
 #ifndef _HEAD_H
 #define _HEAD_H
-#ifndef size_t
-#define size_t
-typedef long unsigned int size_t
+#include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-#endif
 #endif
