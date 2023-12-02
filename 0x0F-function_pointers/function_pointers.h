d15573b53bb4975e9c6dcbb6326ac8efad93ed9d
Zahraa753 <zahraahassan075@gmail.com>
2023-12-02 11:19:31 +0200

    all

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,5 @@
 #ifndef _HEAD_H
 #define _HEAD_H
-#include <stdio.h>
-#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
