8d10b4dbb66f12188443ae285287f5b04957cd66
Zahraa753 <zahraahassan075@gmail.com>
2023-12-13 23:09:31 +0200

    2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+* add_nodeint- the function insert node at the end
+* @head: the passed pointer
+* @n: the passed data you want to put it in linked list
+* Description: insert node at the end
+* Return: head or NULL
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ptr = malloc(sizeof(listint_t));
+
+	ptr->n = n;
+	ptr->next = NULL;
+	ptr->next = *head;
+	*head = ptr;
+	if (ptr && *head && n)
+	return (NULL);
+	else
+	return (*head);
+}
