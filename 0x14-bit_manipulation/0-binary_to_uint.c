c7b484c93321dd4a744ff71c5956f4263a048870
Zahraa753 <zahraahassan075@gmail.com>
2023-12-16 04:24:17 +0200

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	int j;
 	int deci = 0;
 
 	if (b == NULL)
 	return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '1' || b[i] != '0')
+		if (b[i] != '1' && b[i] != '0')
 		return (0);
 	}
 	for (i = 0; b[i] != '\0'; i++)
-	{}
-	for (j = i - 1; i = 0; j != -1; j--; i++)
 	{
-		deci += atoi(b[j]) * (2 ^ i);
+		deci = deci * 2 + (b[i] - '0');
 	}
 	return (deci);
 }
