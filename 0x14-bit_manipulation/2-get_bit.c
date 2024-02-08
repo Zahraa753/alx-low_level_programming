9905d55cf04a24bb617b46d6f5d8aa4fa03241b9
Zahraa753 <zahraahassan075@gmail.com>
2024-02-08 13:02:41 -0800

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,27 +1,13 @@
 #include "main.h"
 /**
-* get_bit- convert to binary
+* get_bit_moreclean- convert to binary
 * @n: the passed decimal number
 * Return: binary number
 */
-int get_bit(unsigned long int n, unsigned int index)
+
+int get_bit_moreclean(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
-	return (-1);
-	char arr[sizeof(n) * 8];
-	int i = 0;
-
-	int repbit = sizeof(n) * 8, puts = 0;
-
-	while (repbit)
-	{
-		if (n & 1L << --repbit)
-	{
-		arr[repbit] = 1;
-		puts++;
-	}
-		else if (puts)
-		arr[repbit] = 0;
-	}
-	return (arr[index]);
+	return -1;
+	return (n >> index & 1);
 }
