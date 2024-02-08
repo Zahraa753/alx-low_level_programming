51fb57c6ee89e1ab321376487cb60aedf04ea200
Zahraa753 <zahraahassan075@gmail.com>
2024-02-08 13:06:11 -0800

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,7 +5,7 @@
 * Return: binary number
 */
 
-int get_bit_moreclean(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 	return -1;
