3f3d28366767c465bfdb30b2b07649bb258ef0f6
Zahraa753 <zahraahassan075@gmail.com>
2024-02-12 20:12:02 +0200

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,15 +2,14 @@
 /**
 * clear_bit- convert to binary
 * @n: the passed decimal number
+* @index: the passed index
 * Return: binary number
 */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	char *m = n;
-	int i = (sizeof(n) * 8);
-	if (index >= sizeof(n) * 8)
-	return -1;
-	*n = ~(01 << index) & *n;
+	if (index >= 32)
+	return (-1);
+	*n = ~(1 << index) & *n;
 	return (1);
 }
