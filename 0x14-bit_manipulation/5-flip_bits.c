ed2605b2f293fbd929edbf4a5455dd0c51628f70
Zahraa753 <zahraahassan075@gmail.com>
2024-02-12 20:29:44 +0200

    solved

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* flip_bits- convert to binary
+* @n: the passed from number
+* @m: the passed to num
+* Return: how many bit to change to reach the other num
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+
+{
+	unsigned long int x = n ^ m;
+	unsigned int sum = 0;
+
+	for (; x > 0; )
+	{
+	if (x & 1ul)
+	sum++;
+	x = x >> 1;
+	}
+}
