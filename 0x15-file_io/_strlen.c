61c151a435168129042cffd31cfe29712c80ae73
Zahraa753 <zahraahassan075@gmail.com>
2024-02-16 23:26:31 +0200

    new

--- 0x15-file_io/_strlen.c
+++ 0x15-file_io/_strlen.c
@@ -2,13 +2,15 @@
 
 /**
 *_strlen- the function gives me the length of string
-*@string: the passed string to be counted
+*@str: the passed string to be counted
 *Return: the length of string
 */
+
 int _strlen(char *str)
 {
 	int num;
 
-	for(num = 0; str[num] != NULL; num++);
+	for (num = 0; str[num] ; num++)
+		;
 	return (num);
 }
