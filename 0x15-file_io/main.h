0bfa618e18033dc11d8e1562b0a23562d1538494
Zahraa753 <zahraahassan075@gmail.com>
2024-02-16 10:30:55 +0200

    all

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
 
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -10,8 +9,10 @@
 #include <fcntl.h>
 #include <string.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _strlen(char *str);
 
 #endif
