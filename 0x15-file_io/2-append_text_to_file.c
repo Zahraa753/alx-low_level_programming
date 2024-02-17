a51bcaed5507edd48afc21f74e8c50c86475fa9d
Zahraa753 <zahraahassan075@gmail.com>
2024-02-17 21:39:41 +0200

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* append_text_to_file- the used function
+* @filename: the passed name of file
+* @text_content: the passed text
+* Return: -1 or 1
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fb;
+	int bit;
+	int writen;
+
+	if (!filename)
+	return (-1);
+	fb = open(filename,O_WRONLY | O_APPEND);
+	if (!fb)
+	return (-1);
+	if (text_content)
+	{
+	for (bit = 0; text_content[bit]; bit++)
+		;
+	writen = write(fb, text_content, bit);
+	if (writen == -1)
+	return (-1);
+	}
+	fclose(fb);
+	return (1);
+}
