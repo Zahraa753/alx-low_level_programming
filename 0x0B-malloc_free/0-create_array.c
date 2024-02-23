d8c2748632f3199cef70484c60835bd4666f0715
Zahraa753 <zahraahassan075@gmail.com>
2024-02-23 21:39:03 +0200

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,15 @@
 char *create_array(unsigned int size, char c)
 {
 	char arr[size];
-	char *ptr;
+	char (*ptr)[size] = arr;
 	int i;
 
-	ptr = malloc(sizeof(char) * size);
 	if (size == 0 || !ptr)
 	return (NULL);
 	for (i = 0; i < size; i++)
 	{
 	arr[i] = c;
 	}
-	ptr = &arr;
+
 	return (ptr);
 }
