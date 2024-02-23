86e87e55b147495639c21e03034f40d843a6f0b3
Zahraa753 <zahraahassan075@gmail.com>
2024-02-23 22:24:30 +0200

    all

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,10 +13,10 @@ char *_strdup(char *str)
 
 	for (i = 0; str[i]; i++)
 	;
-	ptr = malloc(sizeof(char) * i);
+	ptr = malloc((sizeof(char) * i) + 1);
 	if (ptr == NULL || str == NULL)
 	return (NULL);
 	ptr = str;
-	ptr[i] = '\0';
+	ptr[i + 1] = '\0';
 	return (ptr);
 }
